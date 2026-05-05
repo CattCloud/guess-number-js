ec93c328e5af2f4694d26848dd65ff19000d6e86
Erick Verde <erick.verde@unmsm.edu.pe>
2026-05-04 22:38:21 -0500

    feat: Avance 1

---
+++ script.js
@@ -0,0 +1,28 @@
+let nombrePropio = "Erick"; //Creando la variable y la inicialize
+
+let apellidoPaterno ; //Creado
+
+apellidoPaterno= "Verde";//Inicializado
+
+const CURSO= "Code 101";
+
+
+let saludo1 = 'Hola ' + nombrePropio +  " " +  apellidoPaterno + " y estoy en el curso "+CURSO;
+
+let saludoModerno=`Hola me llamo ${nombrePropio} ${apellidoPaterno} y estoy en el curso ${CURSO}`
+
+console.log(saludo1);
+console.log(saludoModerno);
+
+/* let edadUsuario = prompt('¿Cuantos años tienes?');
+
+console.log(edadUsuario);
+console.log(typeof edadUsuario); 
+
+edadUsuario=Number(edadUsuario);
+
+console.log(typeof edadUsuario); 
+
+console.log(edadUsuario+4);
+
+ */
